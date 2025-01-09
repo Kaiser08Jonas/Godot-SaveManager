bbc1affb614b80a01d53f684f40fee6aa3ada3f3
Kaiser08Jonas <137834128+Kaiser08Jonas@users.noreply.github.com>
2025-01-09 19:06:56 +0100

    Impr. save and loading queue
    The save function now call at the end the check_save_queue function to check, if another save should happen. If there is another the save_data function is called, with the information it needs for a save process.
    The same happens in the loading function. This uses the check_loading_queue.

--- save_load.gd
+++ save_load.gd
@@ -17,7 +17,7 @@ signal save_successful(save_name: String)										# Sends a signal if the save 
 signal save_failed(save_name: String, error_message: String)					# Sends a signal if the save has failed.
 signal load_successful(save_name: String)										# Sends a signal if the load was successful.
 signal load_failed(save_name: String, error_message: String)					# Sends a signal if the load has failed.
-signal data_cleanup_successfull(save_name: String)								# Sends a signal if the cleanup was successful.
+signal data_cleanup_successful(save_name: String)								# Sends a signal if the cleanup was successful.
 signal data_cleanup_failed(save_name: String, error_message: String)			# Sends a signal if the cleanup has failed.
 
 
@@ -27,14 +27,17 @@ func _ready() -> void:
 	save_failed.connect(_on_save_failed)
 	load_successful.connect(_on_load_successful)
 	load_failed.connect(_on_load_failed)
-	data_cleanup_successfull.connect(_on_data_cleanup_successfull)
+	data_cleanup_successful.connect(_on_data_cleanup_successful)
 	data_cleanup_failed.connect(_on_data_cleanup_failed)
 
 
 # Function to construct the file path for saving/loading.
 # Returns a string representing the full path to the save file.
-func get_save_path(save_name: String) -> String:
-	return save_directory + save_name + ".save"
+func get_save_path(save_name: String, temporary: bool) -> String:
+	if temporary:
+		return save_directory + save_name + ".tmp"
+	else:
+		return save_directory + save_name + ".save"
 
 
 # Ensures that the storage location exists.
@@ -65,6 +68,12 @@ func save_data(save_name: String, data_to_save: Dictionary) -> bool:
 	
 	is_saving = true # Mark the process as saving.
 	
+	# Ensure that the data to save is a dictionary.
+	if typeof(data_to_save) != TYPE_DICTIONARY:
+		var error_message = "Failed to save data. Data is not a dictionary."
+		save_failed.emit(save_name, error_message)
+		return false
+	
 	# Checks if the folder for the save exists.
 	if !ensure_save_directory():
 		var error_message: String = "Failed to ensure save directory for saving."
@@ -79,7 +88,10 @@ func save_data(save_name: String, data_to_save: Dictionary) -> bool:
 		is_saving = false
 		return false
 	
-	var file : FileAccess = FileAccess.open(get_save_path(save_name), FileAccess.WRITE)
+	var save_path = get_save_path(save_name, false)
+	# Creates a temporary save path. This ensures that existing data is not damaged during loading.
+	var temp_path = get_save_path(save_name, true)
+	var file : FileAccess = FileAccess.open(temp_path, FileAccess.WRITE)
 	
 	# Checks if the file was successfully opened for writing.
 	if file == null:
@@ -92,19 +104,25 @@ func save_data(save_name: String, data_to_save: Dictionary) -> bool:
 	file.store_var(data_to_save)
 	file.close()
 	
+	# Renaming after successful save
+	DirAccess.rename_absolute(temp_path, save_path)
+	
 	save_successful.emit(save_name)
 	
+	is_saving = false # Mark the process as done.
+	
+	check_save_queue()
+	return true
+
+
+func check_save_queue() -> void:
 	# Checks whether further saves should be made.
 	if save_queue.size() > 0:
 		for key in save_queue.keys():
 			var next_item = save_queue[key]
-			save_data(key, next_item) # Recursively save the data if requested during the previous save.
 			save_queue.erase(key)
+			save_data(key, next_item) # Starts the saving process for the next data in the queue.
 			break
-	
-	is_saving = false # Mark the process as done.
-	
-	return true
 
 
 # Load data
@@ -124,16 +142,16 @@ func load_data(save_name: String, data_to_load_into: Dictionary) -> Dictionary:
 		return data_to_load_into
 	
 	# Check if the file exists.
-	if !FileAccess.file_exists(get_save_path(save_name)):
-		var error_message: String = "Failed to load data. File >>" + get_save_path(save_name) + "<< does not exist."
+	if !FileAccess.file_exists(get_save_path(save_name, false)):
+		var error_message: String = "Failed to load data. File >>" + get_save_path(save_name, false) + "<< does not exist."
 		load_failed.emit(save_name, error_message)
 		is_loading = false
 		return data_to_load_into
 	
-	var file: FileAccess = FileAccess.open(get_save_path(save_name), FileAccess.READ)
+	var file: FileAccess = FileAccess.open(get_save_path(save_name, false), FileAccess.READ)
 	# Check if the file was successfully opened for reading.
 	if file == null:
-		var error_message: String = "Failed to load data. Can`t read file >>" + get_save_path(save_name) + "<< ."
+		var error_message: String = "Failed to load data. Can`t read file >>" + get_save_path(save_name, false) + "<< ."
 		load_failed.emit(save_name, error_message)
 		is_loading = false
 		return data_to_load_into
@@ -162,16 +180,9 @@ func load_data(save_name: String, data_to_load_into: Dictionary) -> Dictionary:
 	
 	load_successful.emit(save_name)
 	
-	# Checks whether further saves should be made.
-	if load_queue.size() > 0:
-		for key in load_queue.keys():
-			var next_item = load_queue[key]
-			load_data(key, next_item) # Recursively save the data if requested during the previous save.
-			load_queue.erase(key)
-			break
-	
 	is_loading = false # Mark the loading process as done.
 	
+	check_loading_queue()
 	return data_to_load_into
 
 
@@ -179,7 +190,7 @@ func load_data(save_name: String, data_to_load_into: Dictionary) -> Dictionary:
 func delete_old_data(save_name, loaded_data: Dictionary, data_to_compare: Dictionary) -> bool:
 	# Checks whether the loaded data and the data to be compared are identical.
 	if loaded_data == data_to_compare:
-		data_cleanup_successfull.emit(save_name)
+		data_cleanup_successful.emit(save_name)
 		return false
 	
 	# Add the repuired data to a new dictionary.
@@ -188,7 +199,7 @@ func delete_old_data(save_name, loaded_data: Dictionary, data_to_compare: Dictio
 		if data_to_compare.has(key):
 			new_data[key] = loaded_data[key]
 	
-	var file: FileAccess = FileAccess.open(get_save_path(save_name), FileAccess.WRITE)
+	var file: FileAccess = FileAccess.open(get_save_path(save_name, false), FileAccess.WRITE)
 	
 	# Checks if the file was successfully opened for writing.
 	if file == null:
@@ -199,8 +210,18 @@ func delete_old_data(save_name, loaded_data: Dictionary, data_to_compare: Dictio
 	# Store the new data in the file.
 	file.store_var(new_data)
 	file.close()
-	data_cleanup_successfull.emit(save_name)
+	data_cleanup_successful.emit(save_name)
 	return true
+
+
+func check_loading_queue() -> void:
+	# Checks whether further saves should be made.
+	if load_queue.size() > 0:
+		for key in load_queue.keys():
+			var next_item = load_queue[key]
+			load_queue.erase(key)
+			load_data(key, next_item) # Starts the loading process for the next data in the queue.
+			break
 
 
 # Prints the debug messages
@@ -220,9 +241,9 @@ func _on_load_failed(save_name, error_message):
 	if print_debug_messages:
 		printerr("load failed from >> " + save_name + " <<. " + error_message)
 
-func _on_data_cleanup_successfull(save_name):
+func _on_data_cleanup_successful(save_name):
 	if print_debug_messages:
-		print("data cleanup successfull for >> " + save_name + " <<")
+		print("data cleanup successful for >> " + save_name + " <<")
 
 func _on_data_cleanup_failed(save_name, error_message):
 	if print_debug_messages:
